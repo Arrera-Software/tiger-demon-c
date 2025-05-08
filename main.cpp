c8289aa29b173163a3ca5f1a3bf7d1437fe1c0a2
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-05-08 13:07:25 +0200

    Modif du main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 
     // Met ici l'URL d'un vrai JSON distant pour tester
     CTigerDemon demon("https://arrera-software.fr/depots.json", "six");
+    demon.checkUpdate();
 
-    return a.exec();
+    a.quit();
+    return 1;
 }
