14dc8906ebbf602a0a6e533994992868d964537d
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-05-08 12:58:10 +0200

    Suppression dependance

--- ctigerdemon.cpp
+++ ctigerdemon.cpp
@@ -1,20 +1,12 @@
 #include "ctigerdemon.h"
 
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QEventLoop>
-#include <QUrl>
-
 using namespace std;
 
 CTigerDemon::CTigerDemon(const QString& url, const QString& nameSoft, QObject* parent)
     : QObject(parent)
 {
     QNetworkAccessManager mgr(this);
-    QNetworkRequest req(url);             // <-- c'est bien le constructeur !
+    QNetworkRequest req(url);
     QNetworkReply* reply = mgr.get(req);
 
     QEventLoop loop;
@@ -28,14 +20,8 @@ CTigerDemon::CTigerDemon(const QString& url, const QString& nameSoft, QObject* p
             QJsonObject root = doc.object();
             if (root.contains(nameSoft) && root[nameSoft].isObject()) {
                 contenuJSON = root[nameSoft].toObject();
-            } else {
-                qDebug() << "[CTigerDemon] Le nom" << nameSoft << "n'est pas trouvé dans le JSON.";
             }
-        } else {
-            qDebug() << "[CTigerDemon] Le document JSON n'est pas un objet.";
         }
-    } else {
-        qWarning() << "[CTigerDemon] Erreur réseau : " << reply->errorString();
     }
     reply->deleteLater();
 }
